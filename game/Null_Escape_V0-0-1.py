117ceb126cc8cdb058e84e892a1fb4bc1c0a4ed0
Benji <charles.benji33@gmail.com>
2020-02-07 15:50:28 -0500

    Testing PyCharm

---
+++ game/Null_Escape_V0-0-1.py
@@ -0,0 +1,13 @@
+import pygame
+import random
+import os
+import time
+
+
+pygame.init()
+
+(width, height) = (970, 600)
+
+screen = pygame.display.set_mode((width, height))
+while True:
+    pygame.display.update()
